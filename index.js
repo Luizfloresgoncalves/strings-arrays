80c294e96541e580784bb81bcf04686614b4b096
Luiz Flores Goncalves Neto <luizfloresgoncalvesneto@gmail.com>
2023-08-22 21:46:01 -0300

    criado strings-arrays

---
+++ index.js
@@ -0,0 +1,50 @@
+/* resp a. da undefined pois não foi dado valor a variável
+resp b. foi atribuido valor null para array
+resp c. 11 é o numero do indice da array
+resp d. vai da resultado 3 porque array indice 0 esta com numero 3
+resp e. trocou o número 4 pelo 19 no  do indice 1 
+resp f. aparece o numero 9 no indice da casa 6
+*/
+/* exercicio 2
+a frase ficou toda em letra maiuscula,trocou a letra "a" de marrocos por "i" e colocou o comprimento da frase no caso 29
+*/
+
+// escrita de codigo
+/* exercicio 1
+const nomeUsuario = prompt("Qual seu nome?")
+const emailUsuario = prompt("Qual seu email?")
+console.log(`O email ${emailUsuario} foi cadastrado com sucesso.Seja bem vindo(a), ${nomeUsuario} `)
+*/
+
+/* exercicio 2
+
+a.  const comidaPreferida = ["churrasco", "strogonof", "lasanha", "sopa", "dobradinha",]
+b.  console.log(comidaPreferida)
+console.log(`Essas são minhas comidas preferidas: ${comidaPreferida[0]},
+${comidaPreferida[1]},
+${comidaPreferida[2]},
+${comidaPreferida[3]},
+${comidaPreferida[4]}.`)
+
+c.  const comidaPreferidaUsuario =prompt("Qual sua comida preferida")
+console.log(comidaPreferidaUsuario)
+let i = 0
+comidaPreferida [i+1]= comidaPreferidaUsuario
+console.log(comidaPreferida)*/
+
+/* exercicio 3
+
+const listaDeTarefas = []
+const tarefaUsuario1 = prompt("Qual a primeira tarefa 1? ")
+const tarefaUsuario2 = prompt("Qual a segunda tarefa? ")
+const tarefaUsuario3 = prompt("Qual terceira tarefa? ")
+
+listaDeTarefas[0] = tarefaUsuario1
+listaDeTarefas[1] = tarefaUsuario2
+listaDeTarefas[2] =tarefaUsuario3
+console.log(listaDeTarefas)
+
+const tarefaRealizada = prompt("Qual o indice da tarefa ja realizou? ")
+let i = 0
+listaDeTarefas.splice(i,1)
+console.log(listaDeTarefas)*/
